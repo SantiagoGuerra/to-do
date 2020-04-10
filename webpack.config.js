86142de565c164aea275e42d082d5fdb37391e09
ivanderlich@gmail.com <ivanderlich@gmail.com>
2020-04-10 14:16:09 -0300

    Setup

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,22 @@
-module.exports = {
-  mode: 'development',
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {  
+  //mode: 'development',
+  entry: './src/index.js',
+  output: {
+    filename: 'main.[hash].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: 'Output Management',
+      template: './src/index.html',
+    }),
+  ],
   watch: true,
   module: {
     rules: [
