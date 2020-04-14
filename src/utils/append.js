16aebf2fd1e827459b44392064b9b3ba35076ddc
ivanderlich@gmail.com <ivanderlich@gmail.com>
2020-04-14 18:11:02 -0300

    Create Append functions

---
+++ src/utils/append.js
@@ -0,0 +1,35 @@
+export function appendHTML(query, html, id) {
+  const element = document.querySelector(query);
+  element.id = id;
+  element.innerHTML = html;
+  return html;
+}
+
+function createElement(elementType) {
+  return document.createElement(elementType);
+}
+
+export function appendChild(query, htmlString, id, elementType) {
+  const elem = createElement(elementType);
+  elem.id = id;
+  elem.innerHTML = htmlString;
+  const content = document.querySelector(query);
+  content.appendChild(elem);
+  return true;
+}
+
+export function appendChildToMain(htmlString, id) {
+  appendChild('#content', htmlString, id, 'div');
+  return true;
+}
+
+
+// function renderContent(htmlString, id) {
+//   while (main.firstChild) {
+//     main.removeChild(main.lastChild);
+//   }
+//   const elem = document.createElement('div');
+//   elem.id = id;
+//   elem.innerHTML = htmlString;
+//   main.appendChild(elem);
+// }
