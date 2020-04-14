10b8bea411779f43b4616b506ede28dd42ee1dc0
ivanderlich@gmail.com <ivanderlich@gmail.com>
2020-04-14 18:08:38 -0300

    Load state in DOM

--- src/index.js
+++ src/index.js
@@ -1,8 +1,35 @@
 import './styles/main.scss';
 // import './testing/main';
 import seed from './data/seed';
-//import render from './dom/content';
+import initializeState from './state';
+import {appendHTML, appendChildToMain, appendChild } from './utils/append';
+import projectListTemplate from './templates/projectListTemplate.html';
+import projectTemplate from './templates/projectTemplate.html';
+import taskTemplate from './templates/taskTemplate.html';
 
-seed();
-const anchor = document.querySelector('#content');
-console.log(anchor);
+// <comment> Initialize
+const state = initializeState();
+// </comment>
+
+appendChildToMain(projectListTemplate, 'projectList');
+
+appendChild('#projectList', `<h1>${state.projectList.projects[0].name}</h1>`, state.projectList.projects[0].id, 'div');
+
+appendChild(`#${state.projectList.projects[0].id}`, `<h2>${state.projectList.projects[0].tasks[0].title}</h2>`, state.projectList.projects[0].tasks[0].id, 'div');
+appendChild(`#${state.projectList.projects[0].id}`, `<h2>${state.projectList.projects[0].tasks[1].title}</h2>`, state.projectList.projects[0].tasks[1].id, 'div');
+appendChild(`#${state.projectList.projects[0].id}`, `<h2>${state.projectList.projects[0].tasks[2].title}</h2>`, state.projectList.projects[0].tasks[2].id, 'div');
+
+appendChild('#projectList', `<h1>${state.projectList.projects[1].name}</h1>`, state.projectList.projects[1].id, 'div');
+
+appendChild(`#${state.projectList.projects[1].id}`, `<h2>${state.projectList.projects[1].tasks[0].title}</h2>`, state.projectList.projects[1].tasks[0].id, 'div');
+appendChild(`#${state.projectList.projects[1].id}`, `<h2>${state.projectList.projects[1].tasks[1].title}</h2>`, state.projectList.projects[1].tasks[1].id, 'div');
+appendChild(`#${state.projectList.projects[1].id}`, `<h2>${state.projectList.projects[1].tasks[2].title}</h2>`, state.projectList.projects[1].tasks[2].id, 'div');
+
+appendChild('#projectList', `<h1>${state.projectList.projects[2].name}</h1>`, state.projectList.projects[2].id, 'div');
+
+appendChild(`#${state.projectList.projects[2].id}`, `<h2>${state.projectList.projects[2].tasks[0].title}</h2>`, state.projectList.projects[2].tasks[0].id, 'div');
+appendChild(`#${state.projectList.projects[2].id}`, `<h2>${state.projectList.projects[2].tasks[1].title}</h2>`, state.projectList.projects[2].tasks[1].id, 'div');
+appendChild(`#${state.projectList.projects[2].id}`, `<h2>${state.projectList.projects[2].tasks[2].title}</h2>`, state.projectList.projects[2].tasks[2].id, 'div');
+
+console.log(state);
+
