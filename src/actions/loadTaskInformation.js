dff58c5674c8f15e74464db3accdea8eb0a4914d
Santiago Guerra <guerrasantiago19@gmail.com>
2020-04-15 11:08:34 -0500

    create function to load task information in the side layout

---
+++ src/actions/loadTaskInformation.js
@@ -0,0 +1,13 @@
+import {appendChild} from '../utils/append';
+import TaskInformation from '../components/TaskInformation/index';
+
+export default function loadTaskInformation(task){
+  
+  document.querySelector('.edit-task').innerHTML = ''
+
+  let projectElem = appendChild('.edit-task', TaskInformation(task.title, task.description), task.id, 'div');
+    
+  // projectElem.addEventListener('click', e => {
+  //   console.log(task.title)
+  // })
+}
