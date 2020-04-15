e38310719d9362a9e49dd77cc4242377607a102c
Santiago Guerra <guerrasantiago19@gmail.com>
2020-04-15 11:09:09 -0500

    create TaskInformation component

---
+++ src/components/TaskInformation/index.js
@@ -0,0 +1,12 @@
+export default function TaskInformation(title, description) {
+  return `
+    <div class='task-information'>
+      <h2>
+        ${title}
+      </h2>
+      <p>
+        ${description}
+      </p>
+    </div>
+  `
+}
